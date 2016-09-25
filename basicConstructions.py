1594c79b66fee83846f76fa9610b7372865bfa62
teddybane <the.zugo@gmail.com>
2016-09-25 19:17:23 +0200

    Create basicConstructions.py
    some my ideas how to begin

---
+++ basicConstructions.py
@@ -0,0 +1,7 @@
+#create function which gets True/False boolean value and returns 0 - true 1- fase
+
+#create function wich gets True/False boolean value and returns string '0' = true, '1' = false
+
+#create function wich gets number and returns list of prime numbers <= (less or equal) this number
+
+#create function wchich gets dictionary and returns tuple of lists with keys and values of this dict
